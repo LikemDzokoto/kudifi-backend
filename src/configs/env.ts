50e26f7cdf8beace9ea9236997186eab3d4034a3
LikemDzokoto <likemdzokoto@gmail.com>
2025-08-04 11:40:28 +0000

    rm thirdweb vault

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -5,9 +5,9 @@ const envSchema = z.object({
   // thirdweb
   THIRDWEB_CLIENT_ID: z.string(),
   THIRDWEB_SECRET_KEY: z.string(),
-  THIRDWEB_VAULT_ADMIN_KEY: z.string(),
-  THIRDWEB_VAULT_ACCESS_TOKEN: z.string(),
-  // upstash redis
+  // THIRDWEB_VAULT_ADMIN_KEY: z.string(),
+  // THIRDWEB_VAULT_ACCESS_TOKEN: z.string(),
+
   UPSTASH_REDIS_REST_URL: z.string(),
   UPSTASH_REDIS_REST_TOKEN: z.string(),
 });
